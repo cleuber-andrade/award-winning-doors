87da51124e346422a39417e505a641fc52ee35d5
cleuber-andrade <cleuber_binho@hotmail.com>
2022-01-20 15:07:36 -0300

    update

--- components/Gift.jsx
+++ components/Gift.jsx
@@ -3,8 +3,10 @@ import styles from "../styles/Gift.module.css";
 export default function Gift(){
   return (
     <div className={styles.presente}>
-      <div className={styles.tampa}>Tampa</div>
-      <div className={styles.corpo}>Corpo</div>
+      <div className={styles.tampa}></div>
+      <div className={styles.corpo}></div>
+      <div className={styles.laco1}></div>
+      <div className={styles.laco2}></div>
     </div>
   )
 }
