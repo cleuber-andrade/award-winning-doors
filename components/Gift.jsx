8fc014eee4bff3d99c91851d8b0645ffde636ea0
cleuber-andrade <cleuber_binho@hotmail.com>
2022-01-20 14:56:56 -0300

    update

---
+++ components/Gift.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Gift.module.css";
+
+export default function Gift(){
+  return (
+    <div className={styles.Gift}>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
