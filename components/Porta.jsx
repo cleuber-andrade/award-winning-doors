39774ddd63ef3aaa30a5d19b4777bd64b8121bfe
cleuber-andrade <cleuber_binho@hotmail.com>
2022-01-20 17:16:17 -0300

    update

--- components/Porta.jsx
+++ components/Porta.jsx
@@ -3,7 +3,7 @@ import styles from "../styles/Porta.module.css";
 export default function Porta(props){
   return (
     <div className={styles.porta}>
-
-    </div>
+      <div className={styles.numero}>3</div>
+    </div>  
   )
 }
