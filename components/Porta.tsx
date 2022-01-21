8a1fd5ce20e5d011dafde5173042c6ca47a50031
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-21 18:02:33 -0300

    Update Porta.tsx

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -12,10 +12,10 @@ export default function Porta(props: PortaProps){
 
   return (
     <div className={styles.area}>
-      <div className={`${styles.estrutura} ${selecionada}`}>
+      <div className={`${styles.estrutura} ${styles.selecionada}`}>
         <div className={styles.porta}>
           <div className={styles.numero}>{porta.numero}</div>
-          <div className={`${styles.macaneta} ${selecionada}`}></div>
+          <div className={styles.macaneta}></div>
         </div> 
       </div>
       <div className={styles.chao}></div>
