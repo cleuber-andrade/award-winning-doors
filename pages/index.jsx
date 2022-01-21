c96c312933b52d978ac9c68128a9361c13885649
cleuber-andrade <cleuber_binho@hotmail.com>
2022-01-21 11:19:51 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,9 +4,11 @@ import PortaModel from "../model/porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));  
+  const [texto, setTexto] = useState("...");
 
   return (
-    <div >
+    <div style={{display: "flex", flexDirection: "column"}}>
+      <input type="text" value={texto} onChange={e => setTexto(e.target.value)} />
       <Porta porta={p1}/>      
     </div>
   )
