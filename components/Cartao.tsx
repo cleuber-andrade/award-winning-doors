c3952814a6cef968fad5f0108511cbd2a4cb7e13
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-22 15:39:48 -0300

    update

--- components/Cartao.tsx
+++ components/Cartao.tsx
@@ -1,14 +1,15 @@
 import styles from "../styles/Cartao.module.css";
 
 interface CartaoProps {
-  bgcolor?: string
+  bgcolor?: string,
+  children?: any
 }
 
 export default function Cartao(props: CartaoProps){
   return (
     <div className={styles.cartao} style={{
       backgroundColor: props.bgcolor ?? "#fff" }}>
-      <h1>Fomulário de Início</h1>
+      {props.children}
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import Cartao from "../components/Cartao";
 export default function Formulario() {   
     return (
     <div className={styles.formulario}>
-      <div>
-        <Cartao bgcolor="#c0392c" ></Cartao>
+      <div>       
+        <Cartao bgcolor="#c0392c" >
+          <h1>Porta dos desesperado</h1>
+        </Cartao>
         <Cartao></Cartao>  
       </div>
       <div>
