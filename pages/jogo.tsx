f7e7659b4137ba64b639e0135ab367e26acd3593
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-22 14:38:17 -0300

    update

--- pages/jogo.tsx
+++ pages/jogo.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
 import Porta from "../components/Porta";
-import { atulizarPortas, criarPortas } from "../function/portas";
+import { useState } from "react";
+import { criarPortas, atulizarPortas } from "../function/portas";
 
-export default function Home() {   
+
+export default function Jogo(){
   const [ portas, setPortas ] = useState(criarPortas(5, 2));
 
   const renderizarPortas = () => {
@@ -10,7 +11,7 @@ export default function Home() {
       return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atulizarPortas(portas, novaPorta))}/>
     })
   }
-
+  
   return (
     <div style={{display: "flex"}}>
       {renderizarPortas()}           
