b6febc9c60c2b19b88ee572a94765b865180b5f4
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-22 13:49:08 -0300

    update

--- function/portas.ts
+++ function/portas.ts
@@ -1,7 +1,9 @@
 import PortaModel from "../model/porta"
 
 export const criarPortas = (qtde: number, selecionada: number): PortaModel[] => {
-  return (
-    
-  )
+  return Array.from({length: qtde}, (_, i) => {
+    const numero = i + 1;
+    const temPresente = numero === selecionada;
+    return new PortaModel(numero, temPresente);
+  })
 } 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import Porta from "../components/Porta";
+import { criarPortas } from "../function/portas";
 import PortaModel from "../model/porta";
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));  
 
+  console.log(criarPortas(3, 2));
+
   return (
     <div style={{display: "flex"}}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
-            
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>            
     </div>
   )
 }
