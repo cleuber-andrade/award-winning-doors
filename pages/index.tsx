431aba72e1f9647ad2397e13931dbe31f4cbf1f8
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-22 15:50:19 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
 import styles from "../styles/Formulario.module.css";
 import Cartao from "../components/Cartao";
+import Link from "next/link";
 
 export default function Formulario() {   
     return (
     <div className={styles.formulario}>
       <div>       
         <Cartao bgcolor="#c0392c" >
-          <h1>Porta dos desesperado</h1>
+          <h1>Porta Premiada</h1>
         </Cartao>
         <Cartao></Cartao>  
       </div>
       <div>
         <Cartao></Cartao>
-        <Cartao bgcolor="#28a085"></Cartao>
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`} passHref>
+            <h2 style={{ flex: "1" }}>Iniciar</h2>
+          </Link>          
+        </Cartao>
       </div>
     </div>
   )
