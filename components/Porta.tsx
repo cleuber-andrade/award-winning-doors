7c2f849b5da5461752a8f801533c5db7b944843f
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-22 12:44:34 -0300

    update

--- components/Porta.tsx
+++ components/Porta.tsx
@@ -10,15 +10,25 @@ export default function Porta(props: PortaProps){
   const porta = props.value;
   const selecionada = porta.selecionada ? styles.selecionada : '';
 
-  const alternarSelecao = e => props.onChange(porta.alternarSelecao()); 
+  const alternarSelecao = e => props.onChange(porta.alternarSelecao());
+  const abrir = e => {
+    e.stopPropagation();
+    props.onChange(porta.abrir());
+  }
+  
+  const renderizarPorta = () => {
+    return(
+      <div className={styles.porta}>
+        <div className={styles.numero}>{porta.numero}</div>
+        <div className={styles.macaneta} onClick={abrir}></div>
+      </div> 
+    )
+  }
 
   return (
-    <div className={styles.area} onClick={alternarSelecao}>
+    <div className={styles.area} onClick={alternarSelecao}>      
       <div className={`${styles.estrutura} ${selecionada}`}>
-        <div className={styles.porta}>
-          <div className={styles.numero}>{porta.numero}</div>
-          <div className={styles.macaneta}></div>
-        </div> 
+        {porta.aberta ? false : renderizarPorta()}        
       </div>
       <div className={styles.chao}></div>
     </div> 
