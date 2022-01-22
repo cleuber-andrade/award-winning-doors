132d1019cff612f46c846e8c2102ea4c6bb18a6e
Cleuber Andrade <cleuber_binho@hotmail.com>
2022-01-22 12:50:11 -0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,9 @@ export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));  
 
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
+    <div style={{display: "flex"}}>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
+      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>
       <Porta value={p1} onChange={novaPorta => setP1(novaPorta)}/>       
     </div>
   )
